d15097f77750cf10d28dd8689002d0dfd599cb91
Shien RPi <img21326@gmail.com>
2017-06-26 05:08:11 +0000

    成功初始化

--- Lora.py
+++ Lora.py
@@ -4,34 +4,49 @@ import sys
 import glob
 
 class Lora:
-    chip_cmd = [0x08,0x00,0x00]
-
+    chip_cmd = [0x80,0x00,0x00,0]
+    chip_res = [0xc2,0x01,0x00,0x00]
     def __init__(self):
         self.Device_init()
 
     def Device_init(self):
         self.ports = glob.glob('/dev/tty[A-Za-z]*')
+        dev =""
+        i = 0
         for port in self.ports:
-            dev += i+ "->" + port + ","
+            dev += str(i)+ "->" + port + ","
             i +=1
-        intputport = raw_input('Choose your Device?' + dev)
-        self.ser = serial.Serial(intputport)
-        self.ser.baudrate = 9600
+        intputport = int(raw_input('Choose your Device?' + dev))
+        self.ser = serial.Serial(self.ports[intputport])
+        print self.ports[intputport]
+        self.ser.baudrate = 115200
         self.ser.timeout = 1
         #self.ser.open()
         print 'init Finished'
 
     def transaction(self,cmd):
-        cmd.append(crc(cmd))
-        self.ser.write(serial.to_bytes(cmd))
-        time.sleep(0.5)
-        tx = self.ser.read(self.ser.inWaiting())
-        Re = []
-        for r in tx:
-            Re.append(ord(r))
-        #print Re
-        return self.changeReturn(Re)
-
+        n = cmd[:]
+        n[len(n)-1] = self.crc(n)
+        print 'send:' , n
+        self.ser.write(serial.to_bytes(n))
+        time.sleep(0.04)
+        i=0
+        self.waitCount = 99
+        bytesToRead = self.ser.inWaiting()
+        while True:
+           data = self.ser.read(bytesToRead)
+           i=i+1
+           if len(data)>0 or i>self.waitCount:
+               break
+        #tx = self.ser.read(self.ser.inWaiting())
+        #tx = self.ser.read()
+        #Re = []
+	#print 'tx:',tx.encode('hex')
+        #for r in tx:
+        #    Re.append(ord(r))
+        #print 're:'
+        #return self.changeReturn(Re)
+        print data.encode('hex')
     def changeReturn(self,R):
         print R
 
@@ -44,6 +59,5 @@ class Lora:
         self.ser.close()
 if __name__ == '__main__':
     Lora = Lora()
-    while True:
-        Lora.transaction(power.chip_cmd)
-        time.sleep(2)
+    Lora.transaction(Lora.chip_cmd)
+    Lora.transaction(Lora.chip_res)
